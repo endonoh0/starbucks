0865c49d8447dcc3ae064648cc1f491ed19e52f6
Eric Ho <endonoho@gmail.com>
2020-09-21 23:18:26 -0700

    Sign in users and redirect

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -5,6 +5,7 @@ export const AuthContext = React.createContext({});
 
 export const AuthProvider = ({children}) => {
   const [user, setUser] = useState(null);
+
   return (
     <AuthContext.Provider value={{
       user,
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,25 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+const config = require('../config.json');
 
 import {
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
-  Button
+  Button,
 } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+
 import { AuthContext } from '../AuthProvider';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const {login} = useContext(AuthContext);
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.get(`${config.api.invokeUrl}/user/3`);
+      const resultPassword = res.data.password;
+      const resultEmail = res.data.email;
+      if (resultPassword == password && resultEmail == email) {
+        setEmail("");
+        setPassword("");
+        login();
+      }
+    } catch (error) {
+      console.log(`An error has occured: ${error}`);
+    }
+  }
 
   return (
     <>
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setEmail(text)}
+      value={email}
+      placeholder="Email or username"
+    />
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setPassword(text)}
+      value={password}
+      placeholder="Password"
+    />
     <Button
-      title="login"
-      onPress={() => {
-        login();
-      }}
+      title="Sign in"
+      onPress={handleSubmit}
     />
     </>
   );
